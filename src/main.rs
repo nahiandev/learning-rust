11f35f722158a073fad48e2a6a5193249a6eeae9
Ibne Nahian <evilprince2009@yahoo.com>
2024-11-28 19:16:58 +0600

    palindrome

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", is_palindrome("Hello, world!"));
+}
+
+
+fn is_palindrome(s: &str) -> bool {
+    let s = s.to_lowercase().replace(" ", "");
+    s == s.chars().rev().collect::<String>()
 }
