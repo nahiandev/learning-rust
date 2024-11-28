12cb3844a40c3373ac57bf5098b9971a6de318d8
Ibne Nahian <evilprince2009@yahoo.com>
2024-11-28 23:44:39 +0600

    rocket framework

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
-fn main() {
-    for i in (-2..=50000).step_by(71)
-    {
-        if i % 69 == 0 {
-            print!("{}\n", i);
-        }    
-    }
+// fn main() {
+//     for i in (-2..=50000).step_by(71)
+//     {
+//         if i % 69 == 0 {
+//             print!("{}\n", i);
+//         }    
+//     }
+// }
+
+
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello Rust!"
+}
+
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
 
 
