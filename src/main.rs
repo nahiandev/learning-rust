c25aa71da50d1c8157d6f6a9e9ad6a8927bfac21
Ibne Nahian <evilprince2009@yahoo.com>
2024-11-29 23:18:55 +0600

    borrow

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 fn main() {
-    let s1 = String::from("hello"); // s1 is the owner of this String
-    let s2 = s1; // s2 takes ownership of the String, s1 is no longer valid
+    let s1 = String::from("hello");
 
-    println!("{}", s2); // This works
-    // println!("{}", s1); // This would cause an error because s1 is no longer valid
+    // Immutable borrow
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    // Mutable borrow
+    let mut s2 = String::from("hello");
+
+    change(&mut s2);
+    println!("After change: {}", s2);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(s: &mut String) {
+    s.push_str(", world");
 }
